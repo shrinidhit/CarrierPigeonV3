9a2dc6ca380efd0e2e10bdd4ea8dfa5a767768f7
Haley Pelletier <haley.pelletier@students.olin.edu>
2014-12-17 19:25:55 -0500

    edited bank and home scenes / transitions between

--- Assets/Scripts/Storyline/SwitchScene.cs
+++ Assets/Scripts/Storyline/SwitchScene.cs
@@ -4,18 +4,15 @@ using System.Collections;
 public class SwitchScene : MonoBehaviour {
 
 	public string sceneToSwitch;
-		
+
 	//Loading Building Scene if player goes into building.
 	void OnTriggerEnter (Collider collider) {
 		if (collider.gameObject.name == "Player") {
-			//Object.DontDestroyOnLoad(collider.gameObject);
+			//Object.DontDestroyOnLoad(collider.gameObject);\]
 			GameObject myplayer = GameObject.Find("World/PlayerGroup/Player");
 			Object.Destroy(GameObject.Find("Setting"));
 			Application.LoadLevelAdditive(sceneToSwitch);
-			myplayer.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
-			if (sceneToSwitch != "City") {
-
-			}
+			myplayer.transform.position = new Vector3 (0.0f, 1.0f, 0.0f);
 		}
 	}
 }
